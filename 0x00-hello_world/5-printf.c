2b723fee8b85bf29946cd9a8c5b1f7b776e38efc
Abdulbasit Fasasi <104197245+Kvngfax@users.noreply.github.com>
2023-07-13 18:17:34 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
-int main() {
-    printf("proper grammar, but the outcome is a piece of art\n");
+/*
+    * Display a sentence
+*/
+int main(void) {
+    printf("with proper grammar, but the outcome is a piece of art\n");
     return 0;
 }
