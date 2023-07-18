7b02e25fa7066c3627615638255c7c50e3f51e44
Abdulbasit Fasasi <104197245+Kvngfax@users.noreply.github.com>
2023-07-18 19:16:08 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,13 @@
 */
 void print_alphabet_x10(void)
 {
-int i;
+int count = 0;
 char letter;
 
-for (i = 0; i < 10; i++)
+while (count++ <= 9)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
-{
 _putchar(letter);
-}
-}
 _putchar('\n');
+}
 }
