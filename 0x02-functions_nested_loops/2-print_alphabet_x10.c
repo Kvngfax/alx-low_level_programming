a4576da2e91e5bdfed44a7b108ce995a92357d4a
kvngfax <kvngfax23@gmail.com>
2023-07-18 11:02:30 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*
+* print_alphabet_x10 - Prints the alphabet in lowercase 10 times, followed by a new line.
+*
+*/
+void print_alphabet_x10(void)
+{
+int i;
+char letter;
+
+for (i = 0; i < 10; i++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+}
+_putchar('\n');
+}
