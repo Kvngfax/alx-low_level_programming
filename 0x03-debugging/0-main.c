8bdcf984418d1e1f4c0e88f42abc8192f9c90e76
kvngfax <kvngfax23@gmail.com>
2023-07-20 09:37:06 -0700

    New commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive, negative, or zero
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+i = 0; // Change the value of i to 0 to test the case of 0
+positive_or_negative(i);
+
+return (0);
+}
