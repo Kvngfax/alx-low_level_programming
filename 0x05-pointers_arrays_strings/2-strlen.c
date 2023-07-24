9d2df74cdcce9c1be8ea988e358a9e6d3f516ad0
Abdulbasit Fasasi <104197245+Kvngfax@users.noreply.github.com>
2023-07-24 16:25:07 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,14 +9,14 @@
 
 int _strlen(char *s)
 {
-	     int length = 0;
+int length = 0;
 
-	     while (*s)
-             {
-	            length++;
-	            s++;
-	     }
+while (*s)
+{
+length++;
+s++;
+}
 		
-		    return length;
+return (length);
 }
 
