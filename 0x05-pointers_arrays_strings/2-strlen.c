2a959a3b98e893ccf099186d83c8273af6d30605
kvngfax <kvngfax23@gmail.com>
2023-07-24 08:21:14 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _strlen - return the length of a string
+* @s: char to check
+* Description: this will return the length of a string
+* Return: 0 is success
+*/
+#include "main.h"
+
+int _strlen(char *s)
+{
+	     int length = 0;
+
+	     while (*s)
+             {
+	            length++;
+	            s++;
+	     }
+		
+		    return length;
+}
+
