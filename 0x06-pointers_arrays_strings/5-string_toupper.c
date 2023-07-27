d477b4ae44cefebbaec8475e04bc69cc9a7d6c16
Abdulbasit Fasasi <104197245+Kvngfax@users.noreply.github.com>
2023-07-27 19:01:29 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,10 +11,10 @@ char *string_toupper(char *s)
 	i = 0;
 
 	while (*(s + i))
-	{	
-	if (*(s + i) >= 'a' && *(s + i) <= 'z')
-	*(s + i) -= 'a' - 'A';
-	i++;
-	}
-	return (s);
+	{
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
+*(s + i) -= 'a' - 'A';
+i++;
+}
+return (s);
 }
